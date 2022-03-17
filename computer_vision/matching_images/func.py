6eb4354272d741c937c5a60ef00714f4028e730f
PC <kevinkhang2909@gmail.com>
2022-03-17 17:30:43 +0700

    arcface

--- computer_vision/matching_images/func.py
+++ computer_vision/matching_images/func.py
@@ -12,6 +12,7 @@ import numpy as np
 
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+print(device)
 
 
 class ShopeeNet(nn.Module):
@@ -185,50 +186,47 @@ class AddMarginProduct(nn.Module):
 
 
 class ArcMarginProduct(nn.Module):
-    r"""Implement of large margin arc distance: :
-        Args:
-            in_features: size of each input sample
-            out_features: size of each output sample
-            s: norm of input feature
-            m: margin
-            cos(theta + m)
+    def __init__(self, in_feature=128, out_feature=10575, s=32.0, m=0.50, easy_margin=False):
         """
-    def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False, ls_eps=0.0):
+        :param in_feature:
+        :param out_feature:
+        :param s:
+        :param m:
+        :param easy_margin:
+        """
         super(ArcMarginProduct, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
+        self.in_feature = in_feature
+        self.out_feature = out_feature
         self.s = s
         self.m = m
-        self.ls_eps = ls_eps  # label smoothing
-        self.weight = Parameter(torch.FloatTensor(out_features, in_features))
+        self.weight = Parameter(torch.Tensor(out_feature, in_feature))
         nn.init.xavier_uniform_(self.weight)
 
         self.easy_margin = easy_margin
         self.cos_m = math.cos(m)
         self.sin_m = math.sin(m)
+
+        # make the function cos(theta+m) monotonic decreasing while theta in [0°,180°]
         self.th = math.cos(math.pi - m)
         self.mm = math.sin(math.pi - m) * m
 
-    def forward(self, input, label):
-        # --------------------------- cos(theta) & phi(theta) ---------------------------
-        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+    def forward(self, x, label):
+        # cos(theta)
+        cosine = F.linear(F.normalize(x), F.normalize(self.weight))
+        # cos(theta + m)
         sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
         phi = cosine * self.cos_m - sine * self.sin_m
+
         if self.easy_margin:
             phi = torch.where(cosine > 0, phi, cosine)
         else:
-            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+            phi = torch.where((cosine - self.th) > 0, phi, cosine - self.mm)
 
-        # convert label to one-hot
-        # one_hot = torch.zeros(cosine.size(), requires_grad=True, device='cuda')
-        one_hot = torch.zeros(cosine.size(), device='cuda')
-        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
-        if self.ls_eps > 0:
-            one_hot = (1 - self.ls_eps) * one_hot + self.ls_eps / self.out_features
-
-        # torch.where(out_i = {x_i if condition_i else y_i)
+        #one_hot = torch.zeros(cosine.size(), device='cuda' if torch.cuda.is_available() else 'cpu')
+        one_hot = torch.zeros_like(cosine)
+        one_hot.scatter_(1, label.view(-1, 1), 1)
         output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
-        output *= self.s
+        output = output * self.s
 
         return output
 
@@ -268,3 +266,13 @@ def displayDF(train, path, random=False, COLS=6, ROWS=4):
             plt.axis('off')
             plt.imshow(img)
         plt.show()
+
+
+def f1_score_cal(y_true, y_pred):
+    y_true = y_true.apply(lambda x: set(x.split()))
+    y_pred = y_pred.apply(lambda x: set(x.split()))
+    intersection = np.array([len(x[0] & x[1]) for x in zip(y_true, y_pred)])
+    len_y_pred = y_pred.apply(lambda x: len(x)).values
+    len_y_true = y_true.apply(lambda x: len(x)).values
+    f1 = 2 * intersection / (len_y_pred + len_y_true)
+    return f1
