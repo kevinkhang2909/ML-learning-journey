c60c1da3350e31ba9d4cf344cd22ba6b1b4b703d
PC <kevinkhang2909@gmail.com>
2022-04-20 13:24:48 +0700

    arcface

--- computer_vision/matching_images/func.py
+++ computer_vision/matching_images/func.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import cv2
+from PIL import Image
 from pathlib import Path
 
 path = Path.home() / 'OneDrive - Seagroup/computer_vison/shopee_item_images/'
@@ -58,3 +59,28 @@ def get_data(path, path_img):
     df['target'] = df["label_group"].map(group_dicts)
     df['title_edit'] = df['title'].map(clean_text)
     return df
+
+
+def show_images_horizontally(file):
+    m = len(file) + 1
+    fig, ax = plt.subplots(1, m)
+    fig.set_figwidth(5 * m)
+
+    for a, f in zip(ax, file):
+        file_name, tit = f
+        a.imshow(Image.open(file_name))
+        title_with_return = edit_title(tit)
+        a.title.set_text(title_with_return)
+        a.axis("off")
+    plt.show()
+
+
+def show_image(file):
+    file_name, tit = file
+    fig, ax = plt.subplots(1, 1)
+    title_with_return = edit_title(tit)
+
+    fig.set_figwidth(5)
+    ax.title.set_text(title_with_return)
+    ax.imshow(Image.open(file_name))
+    ax.axis("off")
