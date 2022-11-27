adf68818cf29a51be92ac5484902adc6f795800f
kevin <kevinkhang2909@gmail.com>
2022-11-27 10:20:50 +0700

    grad cam

---
+++ computer_vision/vit_transformers/grad_cam_interpret/modules.py
@@ -0,0 +1,64 @@
+from pytorch_grad_cam import run_dff_on_image, GradCAM
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from PIL import Image
+import numpy as np
+import cv2
+import torch
+from typing import List, Callable, Optional
+
+
+class HuggingfaceToTensorModelWrapper(torch.nn.Module):
+    def __init__(self, model):
+        super(HuggingfaceToTensorModelWrapper, self).__init__()
+        self.model = model
+
+    def forward(self, x):
+        return self.model(x).logits
+
+
+def category_name_to_index(model, category_name):
+    name_to_index = dict((v, k) for k, v in model.config.id2label.items())
+    return name_to_index[category_name]
+
+
+def run_grad_cam_on_image(model: torch.nn.Module,
+                          target_layer: torch.nn.Module,
+                          targets_for_gradcam: List[Callable],
+                          reshape_transform: Optional[Callable],
+                          input_tensor: torch.nn.Module = img_tensor,
+                          input_image: Image = image,
+                          method: Callable = GradCAM):
+    with method(model=HuggingfaceToTensorModelWrapper(model),
+                target_layers=[target_layer],
+                reshape_transform=reshape_transform) as cam:
+        # Replicate the tensor for each of the categories we want to create Grad-CAM for:
+        repeated_tensor = input_tensor[None, :].repeat(len(targets_for_gradcam), 1, 1, 1)
+
+        batch_results = cam(input_tensor=repeated_tensor,
+                            targets=targets_for_gradcam)
+        results = []
+        for grayscale_cam in batch_results:
+            visualization = show_cam_on_image(np.float32(input_image) / 255,
+                                              grayscale_cam,
+                                              use_rgb=True)
+            # Make it weight less in the notebook:
+            visualization = cv2.resize(visualization,
+                                       (visualization.shape[1] // 2, visualization.shape[0] // 2))
+            results.append(visualization)
+        return np.hstack(results)
+
+
+def print_top_categories(model, img_tensor, top_k=5):
+    logits = model(img_tensor.unsqueeze(0)).logits
+    indices = logits.cpu()[0, :].detach().numpy().argsort()[-top_k:][::-1]
+    for i in indices:
+        print(f"Predicted class {i}: {model.config.id2label[i]}")
+
+
+def reshape_transform_vit_huggingface(x):
+    activations = x[:, 1:, :]
+    activations = activations.view(activations.shape[0],
+                                   12, 12, activations.shape[2])
+    activations = activations.transpose(2, 3).transpose(1, 2)
+    return activations
