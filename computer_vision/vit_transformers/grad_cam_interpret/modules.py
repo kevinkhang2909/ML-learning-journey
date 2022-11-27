3a79834353cb0a5acefdd90f9855a1f593cd05fd
Kevin <kevinkhang2909@gmail.com>
2022-11-27 14:29:14 +0700

    vit tranfomers

--- computer_vision/vit_transformers/grad_cam_interpret/modules.py
+++ computer_vision/vit_transformers/grad_cam_interpret/modules.py
@@ -1,5 +1,4 @@
-from pytorch_grad_cam import run_dff_on_image, GradCAM
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from PIL import Image
 import numpy as np
@@ -19,36 +18,31 @@ class HuggingfaceToTensorModelWrapper(LightningModule):
         return self.model(x).logits
 
 
-def category_name_to_index(model, category_name):
-    name_to_index = dict((v, k) for k, v in model.config.id2label.items())
-    return name_to_index[category_name]
-
-
-def run_grad_cam_on_image(model: torch.nn.Module,
-                          target_layer: torch.nn.Module,
-                          targets_for_gradcam: List[Callable],
-                          reshape_transform: Optional[Callable],
-                          input_tensor: torch.nn.Module = img_tensor,
-                          input_image: Image = image,
-                          method: Callable = GradCAM):
-    with method(model=HuggingfaceToTensorModelWrapper(model),
-                target_layers=[target_layer],
-                reshape_transform=reshape_transform) as cam:
-        # Replicate the tensor for each of the categories we want to create Grad-CAM for:
-        repeated_tensor = input_tensor[None, :].repeat(len(targets_for_gradcam), 1, 1, 1)
-
-        batch_results = cam(input_tensor=repeated_tensor,
-                            targets=targets_for_gradcam)
-        results = []
-        for grayscale_cam in batch_results:
-            visualization = show_cam_on_image(np.float32(input_image) / 255,
-                                              grayscale_cam,
-                                              use_rgb=True)
-            # Make it weight less in the notebook:
-            visualization = cv2.resize(visualization,
-                                       (visualization.shape[1] // 2, visualization.shape[0] // 2))
-            results.append(visualization)
-        return np.hstack(results)
+# def run_grad_cam_on_image(model: torch.nn.Module,
+#                           target_layer: torch.nn.Module,
+#                           targets_for_gradcam: List[Callable],
+#                           reshape_transform: Optional[Callable],
+#                           input_tensor: torch.nn.Module = img_tensor,
+#                           input_image: Image = image,
+#                           method: Callable = GradCAM):
+#     with method(model=HuggingfaceToTensorModelWrapper(model),
+#                 target_layers=[target_layer],
+#                 reshape_transform=reshape_transform) as cam:
+#         # Replicate the tensor for each of the categories we want to create Grad-CAM for:
+#         repeated_tensor = input_tensor[None, :].repeat(len(targets_for_gradcam), 1, 1, 1)
+#
+#         batch_results = cam(input_tensor=repeated_tensor,
+#                             targets=targets_for_gradcam)
+#         results = []
+#         for grayscale_cam in batch_results:
+#             visualization = show_cam_on_image(np.float32(input_image) / 255,
+#                                               grayscale_cam,
+#                                               use_rgb=True)
+#             # Make it weight less in the notebook:
+#             visualization = cv2.resize(visualization,
+#                                        (visualization.shape[1] // 2, visualization.shape[0] // 2))
+#             results.append(visualization)
+#         return np.hstack(results)
 
 
 def print_top_categories(model, img_tensor, top_k=5):
@@ -58,7 +52,7 @@ def print_top_categories(model, img_tensor, top_k=5):
         print(f"Predicted class {i}: {model.config.id2label[i]}")
 
 
-def reshape_transform_vit_huggingface(x):
+def reshape_vit_huggingface(x):
     """
     Reshaping to features with the format: batch x features x height x width
     Transformers will sometimes have an internal shape that looks like this:
