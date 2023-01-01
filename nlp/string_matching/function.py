85479d7c3b463450b79f3238fcddba220a40532f
kevin <kevinkhang2909@gmail.com>
2023-01-01 16:50:08 +0700

    string matching

--- nlp/string_matching/function.py
+++ nlp/string_matching/function.py
@@ -6,48 +6,57 @@ from sparse_dot_topn import awesome_cossim_topn
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def clean_text(text):
-    return str(text).lower().strip()
+class TextMatch:
+    """
+    Text Matching Wrapper
+    """
+    def __init__(self, base: list, source: list, top_k: int = 5, similarity_thres: float = .5):
+        self.match_sparse_matrix = None
+        self.base = base
+        self.source = source
+        self.top_k = top_k
+        self.similarity_thres = similarity_thres
 
+    @staticmethod
+    def ngrams_func(string, n=3):
+        string = sub(r'[,-./]|\sBD', r'', string)
+        ngrams = zip(*[string[i:] for i in range(n)])
+        return [''.join(ngram) for ngram in ngrams]
 
-def ngrams_func(string, n=3):
-    string = sub(r'[,-./]|\sBD', r'', string)
-    ngrams = zip(*[string[i:] for i in range(n)])
-    return [''.join(ngram) for ngram in ngrams]
+    def get_matches_df(self):
+        non_zeros = self.match_sparse_matrix.nonzero()
+        sparserows = non_zeros[0]
+        sparsecols = non_zeros[1]
 
+        nr_matches = sparsecols.size
+        left_side = np.empty([nr_matches], dtype=object)
+        right_side = np.empty([nr_matches], dtype=object)
+        similairity = np.zeros(nr_matches)
 
-def get_matches_df(sparse_matrix, base, source):
-    non_zeros = sparse_matrix.nonzero()
+        for index in range(0, nr_matches):
+            left_side[index] = self.base[sparserows[index]]
+            right_side[index] = self.source[sparsecols[index]]
+            similairity[index] = self.match_sparse_matrix.data[index]
 
-    sparserows = non_zeros[0]
-    sparsecols = non_zeros[1]
+        return pd.DataFrame({'base': left_side, 'source': right_side, 'similarity': similairity})
 
-    nr_matches = sparsecols.size
+    def run_match(self):
+        # vectorize
+        t1 = time()
+        vectorizer = TfidfVectorizer(min_df=1, analyzer=self.ngrams_func)
+        maxtrix_base = vectorizer.fit_transform(self.base)
+        maxtrix_source = vectorizer.transform(self.source)
+        print(f"Process vectorize: {round(time() - t1, 2)}s")
 
-    left_side = np.empty([nr_matches], dtype=object)
-    right_side = np.empty([nr_matches], dtype=object)
-    similairity = np.zeros(nr_matches)
-
-    for index in range(0, nr_matches):
-        left_side[index] = base[sparserows[index]]
-        right_side[index] = source[sparsecols[index]]
-        similairity[index] = sparse_matrix.data[index]
-
-    return pd.DataFrame({'base': left_side, 'source': right_side, 'similarity': similairity})
-
-
-def run_match(base, source, top, similarity):
-    t1 = time()
-    vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams_func)
-    maxtrix_base = vectorizer.fit_transform(base)
-    maxtrix_source = vectorizer.transform(source)
-    print(f"process vectorize: {round(time() - t1, 2)}s")
-
-    # match
-    t1 = time()
-    matches = awesome_cossim_topn(maxtrix_base, maxtrix_source.transpose(), top, similarity, use_threads=True, n_jobs=4)
-    matches_df = get_matches_df(matches, base=base, source=source)
-    matches_df['similarity'] = matches_df['similarity'].map(lambda x: round(x, 2))
-    print(f"process optimized: {round(time() - t1, 2)}s")
-    print(f'{len(matches_df.base.unique()):,.0f} skus in BASE match {len(matches_df.source.unique()):,.0f} skus in SOURCE')
-    return matches_df
+        # match
+        t1 = time()
+        self.match_sparse_matrix = awesome_cossim_topn(maxtrix_base, maxtrix_source.transpose(),
+                                                       self.top_k, self.similarity_thres, use_threads=True, n_jobs=4)
+        matches_df = self.get_matches_df()
+        matches_df['similarity'] = matches_df['similarity'].map(lambda x: round(x, 2))
+        matches_df['rank'] = matches_df.groupby(['base'])['source'].transform(lambda x: pd.factorize(x)[0]).add(1)
+        print(f"Process optimized: {round(time() - t1, 2)}s")
+        print(f'{len(matches_df.base.unique()):,.0f} items in BASE '
+              f'match {len(matches_df.source.unique()):,.0f} items in SOURCE '
+              f'with top {self.top_k} match and similarity threshold: {self.similarity_thres}')
+        return matches_df
---
+++ nlp/string_matching/stringmatch.md
@@ -0,0 +1,62 @@
+# Fast String Matching
+Data in the real world is messy. Dealing with messy data sets is painful and burns through time which could be 
+spent analysing the data itself.
+
+This post focuses in on "fuzzy" matching and how this can help to automate significant challenges in 
+a large number of data science workflows through:
+- Deduplication: Finding similar categories or entities in a data set 
+(for example, we may need to combine ‘D J Trump’, ‘D. Trump’ and ‘Donald Trump’ into the same entity).
+
+For the human reader it is obvious that both **D J Trump** and **Donald Trump** are the same name. However for 
+a computer these are completely different making spotting these nearly identical strings difficult.
+
+Some examples about product name:
+```text
+- Giày Thể Thao Nike Zoom 2k Thời Trang vs. Giày Thể Thao Nike Thời Trang
+- iPhone 13 Pro Max 128GB vs. iPhone 13 Pro Max 128 | 256 GB
+- Áo Len Tay Dài Cổ Lọ vs. Áo Len Nữ Tay Dài Cổ Lọ Thời Trang
+- Thú Nhồi Bông Hình Chú Ngựa Xinh Xắn Đáng Yêu vs. Thú nhồi bông hình chú ngựa con đáng yêu
+- Shopee vs. Shopeeee vs. Shoppee
+```
+---
+# 1.The Algorithm
+## 1.1 TF-IDF
+It is a simple algorithm which splits text into "chunks" (ngrams), counts the occurrence 
+of each chunk for a given sample and then applies a weighting to this based on how rare the chunk is across 
+all the samples of a data set. This means that useful words are filtered from the ‘noise’ of more common 
+words which occur within text.
+
+Consider a document containing 100 words in which the word ‘cat’ appears 3 times. 
+The term frequency for ‘cat’ is then (3 / 100) = 0.03. Now, assume we have 10 million documents 
+and the word ‘cat’ appears in one thousand of these. Then, the inverse document 
+frequency  is calculated as log(10,000,000 / 1,000) = 4. Thus, the TF-IDF weight is the product of 
+these quantities: 0.03 * 4 = 0.12.
+
+## 1.2 N-gram
+In our case using words as terms wouldn’t help us much, as most company names only contain one or two words. 
+We will use n-grams: sequences of N contiguous characters.
+```text
+All 3-grams in "iPhone 13 Pro":
+['iPh', 'Pho', 'hon', 'one', 'ne ', 'e 1', ' 13', '13 ', '3 P', ' Pr', 'Pro']
+```
+
+It is noted that a very sparse matrix is generated after the vectorization. The feature space is 16 million 
+(the vocabulary size of the vectorizer is 2²⁴). On average, one company name contains around 4~5 words. 
+However, there is no native sparse matrix multiplication operation (strictly speaking, we are talking about 
+sparse matrix times sparse matrix) in Spark v2.0 directly. Converting the matrices to dense matrices is 
+not memory efficient.
+
+## 1.3 Compressed sparse row (CSR)
+There are several formats to store a sparse matrix, such as Dictionary of Key (DOK), 
+List of lists (LIL), Coordinate list (COO), Compressed sparse row (CSR) and 
+Compressed sparse column (CSC). Because CSR allows fast access and matrix multiplication, it is used in 
+SciPy Sparse matrix dot function:
+![figure 1](fig1-csr-illustration.png)
+
+# 2. Experiment with dataset
+The matches look pretty similar! The cosine similarity gives a good indication of the similarity among item names.
+![figure 2](fig2-data-sample.png)
+
+References:
+1. https://www.sun-analytics.nl/posts/2017-07-26-boosting-selection-of-most-similar-entities-in-large-scale-datasets/
+2. https://github.com/ing-bank/sparse_dot_topn
