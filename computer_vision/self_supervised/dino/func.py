5d7733cff93ae835775009efc8926ebbf195a890
Kevin <kevinkhang2909@gmail.com>
2023-04-19 11:39:22 +0700

    self supervised

--- computer_vision/self_supervised/dino/func.py
+++ computer_vision/self_supervised/dino/func.py
@@ -1,10 +1,12 @@
 from torch.utils.data import Dataset
-from torchvision import transforms, io
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.optim import AdamW
+from torch import optim
 from lightning import LightningModule
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import cv2
 
 
 class Config:
@@ -20,16 +22,31 @@ class Config:
 class ImageData(Dataset):
     def __init__(self, files, image_size=256):
         self.files = files
-        self.randcrop_big = transforms.RandomResizedCrop((image_size, image_size), scale=(0.5, 1.0), antialias=True)
-        self.randcrop_small = transforms.RandomResizedCrop((image_size, image_size), antialias=True)
+        self.randcrop_big = A.Compose([
+            A.RandomResizedCrop(image_size, image_size, scale=(0.5, 1.0)),
+            # A.Normalize(
+            #     mean=[0.485, 0.456, 0.406],
+            #     std=[0.229, 0.224, 0.225],
+            # ),
+            ToTensorV2(),
+        ])
+        self.randcrop_small = A.Compose([
+            A.RandomResizedCrop(image_size, image_size),
+            # A.Normalize(
+            #     mean=[0.485, 0.456, 0.406],
+            #     std=[0.229, 0.224, 0.225],
+            # ),
+            ToTensorV2(),
+        ])
 
     def __len__(self):
         return len(self.files)
 
-    def __getitem__(self, i):
-        img = io.read_image(self.files[i])
-        img1 = self.randcrop_big(img)
-        img2 = self.randcrop_small(img)
+    def __getitem__(self, idx):
+        img = cv2.imread(self.files[idx])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img1 = self.randcrop_big(image=img)['image']
+        img2 = self.randcrop_small(image=img)['image']
         if img.shape[0] == 1:
             img1 = torch.cat([img1] * 3)
             img2 = torch.cat([img2] * 3)
@@ -55,16 +72,24 @@ class CollateFn:
 class ImageOriginalData(Dataset):
     def __init__(self, files, image_size=256):
         self.files = files
-        self.resize = transforms.Resize((image_size, image_size))
+        self.resize = A.Compose([
+            A.Resize(image_size, image_size),
+            # A.Normalize(
+            #     mean=[0.485, 0.456, 0.406],
+            #     std=[0.229, 0.224, 0.225],
+            # ),
+            ToTensorV2(),
+        ])
 
     def __len__(self):
         return len(self.files)
 
-    def __getitem__(self, i):
-        img = io.read_image(self.files[i])
+    def __getitem__(self, idx):
+        img = cv2.imread(self.files[idx])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         if img.shape[0] == 1:
             img = torch.cat([img] * 3)
-        return self.resize(img)
+        return self.resize(image=img)['image']
 
 
 class CollateSingleImage(CollateFn):
@@ -123,12 +148,15 @@ def clip_loss(similarity: torch.Tensor) -> torch.Tensor:
 
 
 class LightningModel(LightningModule):
-    def __init__(self, model, lr, loss_fn, valid_files):
+    def __init__(self, model, loss_fn, valid_files, max_epochs, learning_rate, weight_decay, batch_size):
         super().__init__()
         self.model = model
-        self.lr = lr
         self.loss_fn = loss_fn
         self.valid_files = valid_files
+        self.max_epochs = max_epochs
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+        self.batch_size = batch_size
 
     def common_step(self, batch):
         x1, x2 = batch
@@ -141,11 +169,14 @@ class LightningModel(LightningModule):
 
     def training_step(self, batch):
         loss = self.common_step(batch)
-        self.log(name="train_loss", value=loss, on_step=True, on_epoch=True)
+        self.log(name="train_loss", value=loss, on_epoch=True, on_step=True, prog_bar=True, batch_size=self.batch_size)
         return loss
 
     def validation_step(self, images: torch.FloatTensor, *args) -> None:
         return self.model(images)
 
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        return AdamW(self.model.parameters(), lr=self.lr, weight_decay=1e-3)
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, self.max_epochs, eta_min=1e-6)
+        return {"optimizer": optimizer, "lr_scheduler": lr_scheduler}
+        # return optim.AdamW(self.model.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
