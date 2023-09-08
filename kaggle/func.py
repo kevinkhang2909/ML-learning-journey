bcc612e34551d1746ef1927c92235a49d2480d80
Kevin <kevinkhang2909@gmail.com>
2023-09-08 15:50:44 +0700

    clean codes

--- kaggle/func.py
+++ kaggle/func.py
@@ -1,14 +1,22 @@
 import polars as pl
 
 
-def preprocess(df):
-    df = (
-        df.with_columns(pl.col('timestamp').str.to_datetime(format='%Y-%m-%dT%H:%M:%S%z'))
-        .sort(['timestamp'], descending=True)
-    )
-    return df
+class Preprocess:
+    @staticmethod
+    def format_time(df):
+        df = (
+            df.with_columns(pl.col('timestamp').str.to_datetime(format='%Y-%m-%dT%H:%M:%S%z'))
+            .sort(['timestamp'], descending=True)
+        )
+        return df
+
+    @staticmethod
+    def check_null(df):
+        return df.select(pl.all().is_null().sum()).to_dicts()[0]
+
 
 def print_stats(df):
+    print(df.shape)
     print(df['timestamp'].max().date(), df['timestamp'].min().date())
     print(f"# series: {df['series_id'].n_unique()}")
 
--- kaggle/test.py
+++ kaggle/test.py
@@ -1,35 +1,62 @@
 from pathlib import Path
 import polars as pl
-from func import print_stats, preprocess
+import duckdb
+from func import print_stats, Preprocess
 
 
 path = Path.home() / 'OneDrive - Seagroup/ai/kaggle_dataset/child-mind-institute-detect-sleep-states'
 
 # csv
 print('csv')
-files = [*path.glob('*.csv')]
+file = next(path.glob('train*.csv'))
+query = f"""select * from read_csv_auto('{file}')"""
 df = (
-    pl.read_csv(files[0])
-    .pipe(preprocess)
+    duckdb.sql(query)
+    .pl()
+    .drop_nulls()
     # .to_pandas()
 )
 print_stats(df)
-print(f"# nights: {df['night'].n_unique()}")
 print(f"events: {df['event'].value_counts()}")
+print(Preprocess.check_null(df))
 
-# parquet
-print('parquet')
-files = next(path.glob('train*.parquet'))
-df_full = (
-    pl.read_parquet(files, n_rows=100_000)
-    .pipe(preprocess)
-    # .to_pandas()
+query = f"""
+with base as 
+(
+select series_id
+, {', '.join([f'{i}(timestamp) {i}_timestamp' for i in ['min', 'max']])}
+from df
+group by 1
 )
+
+select *
+, date_diff('day', min_timestamp, max_timestamp) day_diff
+from base
+"""
+df_group = duckdb.query(query)
+# parquet
+# print('parquet')
+# file = next(path.glob('train*.parquet'))
+#
+# query = f"""select * from read_parquet('{file}')"""
+# df = (
+#     duckdb.sql(query)
+#     .pl()
+#     # .pipe(Preprocess.format_time)
+# )
+
+# print_stats(df_full)
+# print(Preprocess.check_null(df_full))
+#
+# len(set(df_full['series_id'].unique()) - set(df['series_id'].unique()))
+#
+# df_train = df_full.join(df, on=['series_id', 'timestamp'], how='left')
+# print(df_train.shape)
+# print(Preprocess.check_null(df_train))
 # print_stats(df_full)
 # check nulls
-# null_col = df.select(pl.all().is_null().sum()).to_dicts()[0]
-# print(null_col)
-#
+
+
 # df_night = df.groupby('night').agg(pl.col('series_id').count()).to_pandas()
 # df_series = df.groupby('series_id').agg(pl.col('event').count()).to_pandas()
 # df.filter(pl.col('night') == 1)
